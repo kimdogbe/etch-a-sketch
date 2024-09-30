f452d87e89dd37c2f3a72968d0bb20d0bac82353
kimdogbe <kimdogbe@gmail.com>
2024-09-30 13:48:14 +0100

    Add 16x16 grid with js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello world")
+const container = document.querySelector(".container");
+let gridRows = [];
+
+for (let i = 0; i < 16; i++){
+  let row = [];
+  let childRow = document.createElement("div");
+  childRow.classList.add("childRow");
+
+  for (let j = 0; j < 16; j++){
+    let childDiv = document.createElement("div");
+    childDiv.style.border = "2px solid black";
+    childDiv.style.backgroundColor = "pink";
+    childDiv.classList.add("gridDiv");
+    childDiv.textContent = `${i} x ${j}`;
+
+    childRow.appendChild(childDiv);
+    row.push(childDiv);
+  }
+  
+  container.appendChild(childRow);
+  gridRows.push(row);
+}
