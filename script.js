f181cd411c983014b142ac55859daebc393cf2a1
kimdogbe <kimdogbe@gmail.com>
2024-09-30 15:12:17 +0100

    Add hover effect and reset button
    Added event listener to change the background color of blocks
    after they have been hovered over. Also add a reset button without
    functionality at the moment.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container");
+const body = document.querySelector("body");
 let gridRows = [];
 
 for (let i = 0; i < 16; i++){
@@ -8,10 +9,9 @@ for (let i = 0; i < 16; i++){
 
   for (let j = 0; j < 16; j++){
     let childDiv = document.createElement("div");
-    childDiv.style.border = "2px solid black";
+    childDiv.style.border = "1px solid black";
     childDiv.style.backgroundColor = "pink";
     childDiv.classList.add("gridDiv");
-    childDiv.textContent = `${i} x ${j}`;
 
     childRow.appendChild(childDiv);
     row.push(childDiv);
@@ -20,3 +20,12 @@ for (let i = 0; i < 16; i++){
   container.appendChild(childRow);
   gridRows.push(row);
 }
+
+addEventListener("mouseover", (event) => {
+  event.target.style.backgroundColor = 'red';
+});
+
+const settingsBtn = document.createElement("button");
+settingsBtn.innerText = "Set grid size";
+settingsBtn.classList.add("settingsBtn");
+body.appendChild(settingsBtn);
