ad3d311de0b86be85a41dcf2742b462b8091f513
kimdogbe <kimdogbe@gmail.com>
2024-09-30 15:36:24 +0100

    Add button eventListener
    Added functionality to run after button has been clicked and
    put the grid creation into a reset function.

--- script.js
+++ script.js
@@ -2,23 +2,27 @@ const container = document.querySelector(".container");
 const body = document.querySelector("body");
 let gridRows = [];
 
-for (let i = 0; i < 16; i++){
-  let row = [];
-  let childRow = document.createElement("div");
-  childRow.classList.add("childRow");
+function resetCanvas(gridSize) {
+  container.innerHTML = ""
 
-  for (let j = 0; j < 16; j++){
-    let childDiv = document.createElement("div");
-    childDiv.style.border = "1px solid black";
-    childDiv.style.backgroundColor = "pink";
-    childDiv.classList.add("gridDiv");
-
-    childRow.appendChild(childDiv);
-    row.push(childDiv);
-  }
+  for (let i = 0; i < gridSize; i++){
+    let row = [];
+    let childRow = document.createElement("div");
+    childRow.classList.add("childRow");
   
-  container.appendChild(childRow);
-  gridRows.push(row);
+    for (let j = 0; j < gridSize; j++){
+      let childDiv = document.createElement("div");
+      // childDiv.style.border = "1px solid black";
+      childDiv.style.backgroundColor = "pink";
+      childDiv.classList.add("gridDiv");
+  
+      childRow.appendChild(childDiv);
+      row.push(childDiv);
+    }
+    
+    container.appendChild(childRow);
+    gridRows.push(row);
+  }
 }
 
 addEventListener("mouseover", (event) => {
@@ -29,3 +33,12 @@ const settingsBtn = document.createElement("button");
 settingsBtn.innerText = "Set grid size";
 settingsBtn.classList.add("settingsBtn");
 body.appendChild(settingsBtn);
+
+settingsBtn.addEventListener("click", (event) => {
+  let gridSize = Number(prompt("Select grid size (max = 100)", "64"));
+  gridSize = gridSize > 100 ? 100 : gridSize;
+  
+  resetCanvas(gridSize);
+});
+
+resetCanvas(20);
